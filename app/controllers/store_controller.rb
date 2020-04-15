eb50467cf58643aa9b32ff12899b0d5e3097f77e
Kamil Owczarek <k.owczarek86@gmail.com>
2020-04-15 14:13:39 +0200

    Added LineItems

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    #@counter = increment_counter
   end
+
+  # private
+
+  # def increment_counter 
+  #   if session[:counter].nil?
+  #     session[:counter] = 0
+  #   end
+  #   session[:counter] += 1
+  # end 
 end
